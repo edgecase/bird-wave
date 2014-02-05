701396413cd3e25742572f41859729fde4a7d588
xandrews <john@neo.com>
2014-02-04 23:29:20 -0500

    Start with a simple json array of species names

--- src/bird_man/service.clj
+++ src/bird_man/service.clj
@@ -4,19 +4,15 @@
               [io.pedestal.service.http.body-params :as body-params]
               [io.pedestal.service.http.route.definition :refer [defroutes]]
               [io.pedestal.service.interceptor :refer [defon-request]]
-              [ring.util.response :as ring-resp]))
-
-(defn about-page
-  [request]
-  (ring-resp/response (format "Clojure %s - served from %s"
-                              (clojure-version)
-                              (route/url-for ::about-page))))
+              [io.pedestal.service.log :as log]
+              [ring.util.response :as ring-resp]
+              [datomic.api :as d :refer (q db)]))
 
 (defn home-page
   [request]
   (ring-resp/response "Hello World!"))
 
-(def datomic-connection nil)
+(defonce datomic-connection nil)
 
 (defn add-datomic-conn [request]
   (assoc request :datomic-conn datomic-connection))
@@ -25,11 +21,17 @@
   "Add a conn reference to each request"
   add-datomic-conn)
 
+(defn species-index [{conn :datomic-conn :as request}]
+  (ring-resp/response
+   (sort (map first (q '[:find ?name
+                         :where [_ :sighting/common-name ?name]]
+                       (db conn))))))
+
 (defroutes routes
   [[["/" {:get home-page}
      ;; Set default interceptors for /about and any other paths under /
-     ^:interceptors [(body-params/body-params) datomic-conn bootstrap/html-body]
-     ["/about" {:get about-page}]]]])
+     ^:interceptors [(body-params/body-params) datomic-conn bootstrap/json-body]
+     ["/species" {:get species-index}]]]])
 
 ;; Consumed by bird-man.server/create-server
 ;; See bootstrap/default-interceptors for additional options you can configure
