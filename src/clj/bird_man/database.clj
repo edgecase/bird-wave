b5443f27cb8779924360a9cb51b7a6f94f5ca76e
xandrews <john@neo.com>
2014-02-17 22:35:59 -0500

    Create a db partition for each taxon
    - Store taxon entity in partition
    - Store sighting entities in same partition
    - Results in significantly better query times

--- src/clj/bird_man/database.clj
+++ src/clj/bird_man/database.clj
@@ -1,10 +1,20 @@
 (ns bird-man.database
   (:require [bird-man.import :as import]
             [datomic.api :as d :refer (db q)]
+            [clojure.string :as cs]
+            [clojure.set :as cset]
             ;; [clojure.tools.trace :refer (deftrace trace)]
             ))
 
-(defn existing-taxons [db taxons]
+(defn partition-name
+  "Return partition name as keyword given a taxonomic order string"
+  [order]
+  (keyword (str "taxon/order-"
+                (cs/trim (cs/replace order #"\." "-")))))
+
+(defn existing-taxons
+  "Return a map from taxonomic order to :db/id"
+  [db taxons]
   (into {}
         (q '[:find ?taxonomic-order ?taxon
              :in $ [?taxonomic-order]
@@ -14,19 +24,29 @@
            (map #(vector (:taxon/order %)) taxons))))
 
 (defn transact-rows [conn pairs]
-  (let [taxons (existing-taxons (db conn) (map first pairs))
+  (let [taxons (map first pairs)
+        existing-taxons (existing-taxons (db conn) taxons)
+        partitions-to-create (cset/difference (set (map :taxon/order taxons))
+                                              (set (keys existing-taxons)))
+        new-partitions-tx (for [order partitions-to-create]
+                            {:db/id (d/tempid :db.part/db)
+                             :db/ident (partition-name order)
+                             :db.install/_partition :db.part/db})
         tx-data (for [[taxon sighting] pairs]
-                  (let [taxon-id (get taxons (:taxon/order taxon))]
-                    (if taxon-id
-                      [(assoc sighting
-                         :db/id (d/tempid :db.part/user)
-                         :sighting/taxon taxon-id)]
-                      (let [tmp-taxon-id (d/tempid :db.part/user)]
-                        [(assoc taxon
-                           :db/id tmp-taxon-id)
-                         (assoc sighting
-                           :db/id (d/tempid :db.part/user)
-                           :sighting/taxon tmp-taxon-id)]))))]
+                   (let [taxon-id (get existing-taxons (:taxon/order taxon))]
+                     (if taxon-id
+                       [(assoc sighting
+                          :db/id (d/tempid (d/part taxon-id))
+                          :sighting/taxon taxon-id)]
+                       (let [part (partition-name (:taxon/order taxon))
+                             tmp-taxon-id (d/tempid part)]
+                         [(assoc taxon
+                            :db/id tmp-taxon-id)
+                          (assoc sighting
+                            :db/id (d/tempid part)
+                            :sighting/taxon tmp-taxon-id)]))))]
+    (when (not-empty new-partitions-tx)
+      @(d/transact conn new-partitions-tx))
     @(d/transact conn (flatten tx-data))))
 
 (defn seed-rows [conn {:keys [seed-file batch-size skip-rows nth-row]}]
