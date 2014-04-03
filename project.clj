cb0921b4fc5455ff0d3482dedbc51b1865715092
xandrews <john@neo.com>
2014-04-03 15:03:29 -0400

    Tidy up project.clj file

--- project.clj
+++ project.clj
@@ -1,22 +1,16 @@
 (defproject bird-man "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Watch bird migrations"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [io.pedestal/pedestal.service "0.2.2"]
                  [io.pedestal/pedestal.service-tools "0.2.2"]
-                 [hiccup "1.0.5"]
                  [com.datomic/datomic-pro "0.9.4609" :exclusions [org.slf4j/slf4j-nop]]
-                 [org.clojure/tools.namespace "0.2.4"]
                  [enlive "1.1.4"]
-                 [com.novemberain/validateur "1.5.0"]
-                 [criterium "0.4.3"]
                  [org.clojure/clojurescript "0.0-2173"]
                  [om "0.5.2"]
-                 [secretary "1.0.1"]
-                 [datomic-schema-grapher "0.0.1"]
-                 [ankha "0.1.1"]]
+                 [secretary "1.0.1"]]
   :plugins      [[lein-cljsbuild "1.0.2"]
                  [datomic-schema-grapher "0.0.1"]
                  [ohpauleez/lein-pedestal "0.1.0-beta10"]]
@@ -36,7 +30,10 @@
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :aliases {"run-dev" ["trampoline" "run" "-m" "bird-man.server/run-dev"]}
-  :profiles {:dev {:dependencies [[io.pedestal/pedestal.jetty "0.2.2"]]
+  :profiles {:dev {:dependencies [[io.pedestal/pedestal.jetty "0.2.2"]
+                                  [datomic-schema-grapher "0.0.1"]
+                                  [ankha "0.1.1"]
+                                  [org.clojure/tools.namespace "0.2.4"]]
                    :source-paths ["dev"]}}
 
   :pedestal {:server-ns "bird-man.server"
