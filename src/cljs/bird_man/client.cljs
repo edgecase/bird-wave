e7a1a5afbd6e21f6b91bec9595085796beea32d9
xandrews <john@neo.com>
2014-05-16 15:18:34 -0400

    WIP begin to extract namespaces

--- src/cljs/bird_man/client.cljs
+++ src/cljs/bird_man/client.cljs
@@ -8,7 +8,9 @@
             [goog.events :as events]
             [ankha.core :as ankha]
             [cljs.core.async :as async :refer (chan put! <! timeout)]
-            [arosequist.om-autocomplete :as ac])
+            [arosequist.om-autocomplete :as ac]
+            [bird-man.map :refer (plot init-axis color active-state zoom zoom-duration
+                                       svg-dim state-to-activate active-attrs)])
 
   (:import goog.History
            goog.history.EventType))
@@ -17,79 +19,19 @@
   (if js/window.__birdman_debug__
     (.log js/console (pr-str args))))
 
-(def svg-dim {:width 800 :height 500})
-(def key-dim {:width 10 :height 200})
-(def key-bg-dim {:width 45 :height 210})
-(def max-freq 5)
-(def zoom-duration 550)
-(def projection ( -> js/d3 (.geo.albersUsa) (.scale 940) (.translate (array (+ 10 (/ (:width svg-dim) 2)) (/ (:height svg-dim) 2)))))
-(def path ( -> js/d3 (.geo.path) (.projection projection)))
-(def color ( -> js/d3.scale
-                (.quantile)
-                (.domain (array 0 max-freq))
-                (.range (-> (aget js/colorbrewer.YlGnBu "9") (.reverse)))))
-(def months ( -> (js/d3.time.scale)
-                 (.domain (array (new js/Date 2012 10 15) (new js/Date 2013 10 15)))
-                 (.range (array 0 (- (:width svg-dim) 10)))))
-(def month-axis )
-(def key-scale ( -> js/d3.scale
-                    (.linear)
-                    (.domain (array max-freq 0))
-                    (.range (array 0 (:height key-dim)))))
-(def key-axis ( -> (js/d3.svg.axis)
-                   (.scale key-scale)
-                   (.orient "left")
-                   (.tickValues (color.quantiles))
-                   (.tickFormat (js/d3.format ".1f"))))
-(defn handle-zoom []
-  (-> js/d3
-      (.selectAll "g.topo")
-      (.style "stroke-width" (str (/ 1.2 (aget js/d3 "event" "scale")) "px"))
-      (.attr "transform" (str "translate(" (aget js/d3 "event" "translate") ") scale(" (aget js/d3 "event" "scale") ")"))))
-(def zoom ( -> js/d3.behavior
-               (.zoom)
-               (.translate (array 0 0))
-               (.scale 1)
-               (.scaleExtent (array 1 8))
-               (.on "zoom" handle-zoom)))
-
-(defonce freq-by-county (atom {}))
-(defn target [] (.-target (.-event js/d3)))
-(defn state-to-activate [] (.select js/d3 (target)))
-(defn active-state [] (.select js/d3 ".active"))
-(defn active-attrs [el]
-  (let [bounds (.bounds path el)
-        width (:width svg-dim)
-        height (:height svg-dim)
-        dx (- (aget bounds 1 0) (aget bounds 0 0))
-        dy (- (aget bounds 1 1) (aget bounds 0 1))
-        x  (/ (+ (aget bounds 0 0) (aget bounds 1 0)) 2)
-        y  (/ (+ (aget bounds 0 1) (aget bounds 1 1)) 2)
-        scale (/ 0.9 (js/Math.max (/ dx width) (/ dy height)))
-        translate (array (- (/ width 2) (* scale x)) (- (/ height 2) (* scale y)))]
-    {:scale scale :translate translate}))
 
 (def model (atom {:current-taxon nil
                   :time-period nil     ; selected month
                   :taxonomy []      ; all taxons
-                  :sightings {}     ; sightings for selected taxon, grouped by time-period
-                  }))
-
-(defn changed? [key old new]
-  (not= (get old key) (get new key)))
+                  :frequencies {}}))
 
 (declare update-counties)
 
-(def most-recent-fetch nil)
-
 (defn update-map! [model]
-  (let [{:keys [current-taxon time-period]} @model]
-    (let [url (str "species/" current-taxon "/" time-period)]
-      (when (and current-taxon
-                 time-period
-                 (not= most-recent-fetch url))
-        (js/d3.json url update-counties)))))
-
+  (let [{:keys [current-taxon time-period]} @model
+        url (str "species/" current-taxon "/" time-period)]
+    (when (and current-taxon time-period)
+      (js/d3.json url (update-counties model)))))
 
 (defn taxon-path [taxon]
   (str "/#/taxon/" taxon))
@@ -179,41 +121,36 @@
             (dom/g #js {:className "axis"})))))
     om/IDidMount
     (did-mount [_]
-      (-> js/d3
-          (.select ".axis")
-          (.call (-> (js/d3.svg.axis)
-                     (.scale months)
-                     (.tickFormat (js/d3.time.format "%B"))))))))
+      (init-axis ".axis"))))
 
 
 
 (defn build-key [state county]
   (apply str (interpose "-" [state county])))
 
-(defn populate-freqs [stats]
-  (reset! freq-by-county
-          (into {}
-                (map (fn [s]
-                       [(build-key (aget s "state") (aget s "county"))
-                        (/ (aget s "total") (aget s "sightings"))])
-                     stats))))
+(defn populate-freqs [model stats]
+  (om/update! model :frequencies
+              (into {}
+                    (map (fn [s]
+                           [(build-key (aget s "state") (aget s "county"))
+                            (/ (aget s "total") (aget s "sightings"))])
+                         stats))))
 
-(defn freq-for-county [data]
+(defn freq-for-county [{:keys [frequencies]} data]
   (let [p (aget data "properties")
-       st (str "US-" (aget p "state"))
-       cty (first (cs/split (aget p "county") " "))
-       keystr (build-key st cty)
-       freq (@freq-by-county keystr)]
+        st (str "US-" (aget p "state"))
+        cty (first (cs/split (aget p "county") " "))
+        keystr (build-key st cty)
+        freq (get frequencies keystr)]
   (if freq freq 0.0)))
 
-(defn freq-duration [data]
-  (+ (* 100 (freq-for-county data)) 200))
+(defn freq-duration [frequencies]
+  (fn [data]
+    (+ (* 100 (freq-for-county @frequencies data)) 200)))
 
-(defn freq-color [data]
-  (color (freq-for-county data)))
-
-(defn list-birds [species]
-  )
+(defn freq-color [frequencies]
+  (fn [data]
+    (color (freq-for-county @frequencies data))))
 
 (defn get-birds [model]
   (js/d3.json "species"
@@ -221,15 +158,16 @@
                 (let [taxonomy (map keywordize-keys (js->clj species))]
                   (om/update! model :taxonomy (vec taxonomy))))))
 
-(defn update-counties [results]
-  (log :update-counties)
-  (populate-freqs results)
-  ( -> js/d3
-       (.selectAll "path.county")
-       (.transition)
-       (.duration freq-duration)
-       (.style "fill" freq-color)
-       (.style "stroke" freq-color)))
+(defn update-counties [model]
+  (fn [results]
+    (log :update-counties)
+    (populate-freqs model results)
+    ( -> js/d3
+         (.selectAll "path.county")
+         (.transition)
+         (.duration (freq-duration model))
+         (.style "fill" (freq-color model))
+         (.style "stroke" (freq-color model)))))
 
 (defn reset [svg]
   (.classed (active-state) "active" false)
@@ -257,68 +195,6 @@
 (defn prevent-zoom-on-drag []
   (let [e (.-event js/d3)]
     (when (.-defaultPrevented e) (.stopPropagation e))))
-
-(defn plot [svg us]
-  (let [s-width (:width svg-dim)
-        s-height (:height svg-dim)
-        k-width (:width key-dim)
-        k-height (:height key-dim)
-        b-width (:width key-bg-dim)
-        b-height (:height key-bg-dim)]
-    (-> svg
-        (.append "rect")
-        (.classed "background" true)
-        (.attr "width" s-width)
-        (.attr "height" s-height)
-        (.on "click" #(reset svg)))
-    (-> svg
-        (.append "g")
-        (.classed "topo" true)
-        (.selectAll "path")
-        (.data (aget (js/topojson.feature us (aget us "objects" "counties")) "features"))
-        (.enter)
-        (.append "path")
-        (.classed "county" true)
-        (.attr "d" path))
-    (-> svg
-        (.append "g")
-        (.classed "topo" true)
-        (.selectAll "path")
-        (.data (aget (js/topojson.feature us (aget us "objects" "states")) "features"))
-        (.enter)
-        (.append "path")
-        (.classed "state" true)
-        (.attr "d" path)
-        (.on "click" #(zoom-state svg %)))
-    (-> svg
-        (.call zoom)
-        (.call (.-event zoom)))
-    (-> svg
-        (.append "rect")
-        (.classed "axis-background" true)
-        (.attr "width" b-width)
-        (.attr "height" b-height)
-        (.attr "rx" 4)
-        (.attr "ry" 4)
-        (.attr "transform", (str "translate(" (- s-width b-width 5) "," (- s-height b-height) ")")))
-    (-> svg
-        (.append "text")
-        (.classed "legend" true)
-        (.text "Scale represents the ratio of birds sighted to number of sightings")
-        (.attr "transform", (str "translate(" (- s-width 390) "," (- s-height 5) ")")))
-    (-> svg
-        (.append "g")
-        (.classed "axis" true)
-        (.call key-axis)
-        (.attr "transform", (str "translate(" (- s-width k-width 10) "," (- s-height k-height 5) ")"))
-        (.selectAll "rect")
-        (.data (-> (.range color) (.map #(.invertExtent color %))))
-        (.enter)
-        (.append "rect")
-        (.attr "height" #(- k-height (key-scale (- (nth % 1) (nth % 0)))))
-        (.attr "width" 8)
-        (.attr "y" #(key-scale (nth % 1)))
-        (.style "fill" #(nth (.range color) %2)))))
 
 (defn draw-map [svg]
   (js/d3.json "data/us.json"
